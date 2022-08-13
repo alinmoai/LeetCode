208868cf47f359c7e305e50fb7021f01b3403153
alinmoai <alinmoai@gmail.com>
2022-08-14 04:31:50 +0800

    no message

--- Algorithm and Data Structers/Java/PriorityQueue.java
+++ Algorithm and Data Structers/Java/PriorityQueue.java
@@ -1,7 +1,8 @@
 class Solution {
     public void example () {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-
+        // 一樣是從大到小
+        pq = new PriorityQueue<>((a,b)->b-a);   
         pq.offer(1);
         pq.peek();
         pq.remove(1);
